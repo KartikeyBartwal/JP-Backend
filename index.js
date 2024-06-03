c29e3a4645b3544781ec796b595b657b9a5aa14e
Kartikey Bartwal <bartwalkartikey1@gmail.com>
2024-06-03 17:27:06 +0530

    all files added

---
+++ index.js
@@ -0,0 +1,29 @@
+require('dotenv').config()
+console.log(process.env)
+
+const express = require("express")
+const app = express()
+
+app.get('/' , (req , res) =>{
+    res.send("Hello World!")
+}); 
+
+app.get('/twitter' , (req , res) =>{
+    res.send("Its X now")
+});
+
+app.get('/login' , (req , res) =>{
+    res.send("<h1> Thank you for logging </h1>")
+});
+
+app.get('/youtube' , (req , res)=>{
+    res.send("Pikachu");
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`);
+    console.log("Pikachu")
+});
+
+
+console.log("Chai aur Code")
