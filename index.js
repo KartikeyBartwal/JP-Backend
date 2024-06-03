8738cada920656b1f223f3b3c191fe36f2cab336
Kartikey Bartwal <bartwalkartikey1@gmail.com>
2024-06-03 17:36:52 +0530

    changes made

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require("express")
 const app = express()
 
 app.get('/' , (req , res) =>{
-    res.send("Hello World!")
+    res.send("You're Gay!")
 }); 
 
 app.get('/twitter' , (req , res) =>{
